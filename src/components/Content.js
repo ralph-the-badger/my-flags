99d51ee255841e987806fe789df74e6f8701f31d
ralph_the_badger <badger_rr@gmx.de>
2023-08-06 21:08:33 +0200

    commit project

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,13 +3,20 @@ import Country from "./country/Country";
 function Content({ countries }) {
   return (
     <div className="content-container">
-      <ul className="content-container-list">
-        {countries.map((country) => (
-          <li key={country.cca2}>
-            <Country country={country} />
-          </li>
-        ))}
-      </ul>
+      {countries.length === 0 ? (
+        <p className="no-countries">
+          There are no countries that match your filter or your search. Please
+          check your spelling.
+        </p>
+      ) : (
+        <ul className="content-container-list">
+          {countries.map((country) => (
+            <li key={country.cca2}>
+              <Country country={country} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
